e646a529b755808a393fcfb83718f4cc5a8986de
bsef18m011-MuhammadAdeel <bsef18m011-MuhammadAdeel@users.noreply.github.com>
2021-05-21 10:05:52 +0500

    Updated Registration

--- Tailor_Management_System/app/src/main/java/com/example/tailormanagementsystem/Order.java
+++ Tailor_Management_System/app/src/main/java/com/example/tailormanagementsystem/Order.java
@@ -1,7 +1,17 @@
 package com.example.tailormanagementsystem;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Date;
 
-public class Order {
+public class Order extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
 
     int Id;
     int CustomerID;
