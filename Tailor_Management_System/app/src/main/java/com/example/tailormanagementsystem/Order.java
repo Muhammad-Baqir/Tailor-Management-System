afaab8ff43d9aec2a51779f8c6b6ac95ec4fb0c4
BSEF18M024MuhammadSoban <bsef18m024@pucit.edu.pk>
2021-05-27 14:13:42 +0500

    Updated Order

--- Tailor_Management_System/app/src/main/java/com/example/tailormanagementsystem/Order.java
+++ Tailor_Management_System/app/src/main/java/com/example/tailormanagementsystem/Order.java
@@ -1,70 +1,99 @@
 package com.example.tailormanagementsystem;
+import android.content.ContentValues;
+import android.database.Cursor;
+
 import java.util.Date;
 
-public class Order {
+public class Order extends Table {
+    static public final String TABLE_NAME = "OrderTable";
+    static public final String CUSTOMER_ID = "CustomerId";
+    static public final String TOTAL_AMOUNT= "TotalAmount";
+    static public final String REMAINING_AMOUNT = "RemainingAmount";
+    static public final String ORDER_DATE = "OrderDate";
+    static public final String DEADLINE = "Deadline";
+    static public final String STATUS = "Status";
 
-    int Id;
-    int CustomerID;
-    int TotalAmount;
-    int RemainingAmount;
-    Date OrderDate;
-    Date Deadline;
+    Integer CustomerID;
+    Integer TotalAmount;
+    Integer RemainingAmount;
+    String OrderDate;
+    String Deadline;
     String Status;
 
-    public Order(int id,int customerID,Date orderDate,Date deadline,int totalAmount,int remainingAmount,String status) {
+    public Order(Integer id ,Integer customerID, Integer totalAmount, Integer remainingAmount, String orderDate, String deadline, String status) {
         Id = id;
-        CustomerID=customerID;
-        OrderDate=orderDate;
-        Deadline=deadline;
-        TotalAmount=totalAmount;
-        RemainingAmount=remainingAmount;
-        Status=status;
+        CustomerID = customerID;
+        TotalAmount = totalAmount;
+        RemainingAmount = remainingAmount;
+        OrderDate = orderDate;
+        Deadline = deadline;
+        Status = status;
     }
 
-    public int getId() {
-        return Id;
-    }
-
-    public void setId(int id) {
-        Id = id;
-    }
-    public int getCustomerID() {
+    public Integer getCustomerID() {
         return CustomerID;
     }
 
-    public void setCustomerID(int customerID) {
+    public void setCustomerID(Integer customerID) {
         CustomerID = customerID;
     }
-    public int getTotalAmount() {
+
+    public Integer getTotalAmount() {
         return TotalAmount;
     }
 
-    public void setTotalAmount(int totalAmount) {
+    public void setTotalAmount(Integer totalAmount) {
         TotalAmount = totalAmount;
     }
-    public int getRemainingAmount() {
+
+    public Integer getRemainingAmount() {
         return RemainingAmount;
     }
 
-    public void setRemainingAmount(int remainingAmount) {
+    public void setRemainingAmount(Integer remainingAmount) {
         RemainingAmount = remainingAmount;
     }
-    public Date getOrderDate() {
+
+    public String getOrderDate() {
         return OrderDate;
     }
 
-    public void setOrderDate(Date orderDate) {
+    public void setOrderDate(String orderDate) {
         OrderDate = orderDate;
     }
-    public Date getDeadline() {
+
+    public String getDeadline() {
         return Deadline;
     }
-    public void setDeadline(Date deadline) {
+
+    public void setDeadline(String deadline) {
         Deadline = deadline;
     }
+
     public String getStatus() {
         return Status;
     }
-    public void setStatus(String status) { Status = status; }
 
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    //Override Methods
+    static public Table newObject(Cursor cursor) {
+        return new Order(cursor.getInt(0),cursor.getInt(1),cursor.getInt(2),cursor.getInt(3),cursor.getString(4),cursor.getString(5),cursor.getString(6));
+    }
+
+
+    static public ContentValues getContentValues(Order order) {
+        ContentValues cv = new ContentValues();
+
+        cv.put(CUSTOMER_ID, order.CustomerID);
+        cv.put(DEADLINE, order.Deadline);
+        cv.put(ORDER_DATE, order.OrderDate);
+        cv.put(REMAINING_AMOUNT, order.RemainingAmount);
+        cv.put(TOTAL_AMOUNT, order.TotalAmount);
+        cv.put(STATUS, order.Status);
+
+        return cv;
+    }
 }
